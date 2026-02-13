ceddf7d7e74ea0e36edb8c06fc7bf90b327747e2
gollapallivamsi <143597207+gollapallivamsi@users.noreply.github.com>
2026-02-13 20:45:39 +0530

    Update main.tf

--- aws_instance/main.tf
+++ aws_instance/main.tf
@@ -2,10 +2,11 @@ resource "aws_instance" "example" {
   ami             = var.ami 
   instance_type   = var.instance_type  #"t2.micro"
   key_name        = var.key_name  #"your-keypair-name"
-  security_groups = var.security_groups #[aws_security_group.ec2_sg.name]
+  security_group_ids = var.security_groups #[aws_security_group.ec2_sg.name]
 
   tags = {
     Environment = "Dev"
     Appname     = "wiergallery"
   }
 }
+
