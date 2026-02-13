5c587068af4f52a1a51344fa7cbdd0c5c4da7eec
gollapallivamsi <143597207+gollapallivamsi@users.noreply.github.com>
2026-02-13 20:18:03 +0530

    Refactor Terraform provider configuration

--- .github/workflows/aws_instance/provider.tf
+++ .github/workflows/aws_instance/provider.tf
@@ -12,12 +12,3 @@ terraform {
   }
 }
 
-############################################
-# AWS Provider Block
-############################################
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "YOUR_ACCESS_KEY"
-  secret_key = "YOUR_SECRET_KEY"
-
-}
